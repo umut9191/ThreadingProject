cd2501b3bf40fa89e9cae5577b3d556d85c3ab62
umut9191 <umutsurmeli91@gmail.com>
2022-01-23 20:52:23 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by Mac on 23.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
